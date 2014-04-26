d78521c267b91d34f4907769f77050ae8049ad8a
Adrian <alonsus91@gmail.com>
2014-04-26 14:00:01 +0200

    first version register login

--- src/MIW/PublicBundle/Controller/UserController.php
+++ src/MIW/PublicBundle/Controller/UserController.php
@@ -9,24 +9,48 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use FOS\UserBundle\Controller\RegistrationController as BaseController;
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\Exception\AccountStatusException;
 
-class UserController extends Controller
-{
-    /**
-     * @Route("/registrarse")
-     * @Template("MIWPublicBundle:User:login.html.twig");
-     */
-    public function registerAction()
-    {
-        return array();
+class UserController extends BaseController {
+
+    public function registerAction() {
+        $form = $this->container->get('fos_user.registration.form');
+        $formHandler = $this->container->get('fos_user.registration.form.handler');
+        $confirmationEnabled = $this->container->getParameter('fos_user.registration.confirmation.enabled');
+
+        $process = $formHandler->process($confirmationEnabled);
+        if ($process) {
+            $user = $form->getData();
+
+            $authUser = false;
+            if ($confirmationEnabled) {
+                $this->container->get('session')->set('fos_user_send_confirmation_email/email', $user->getEmail());
+                $route = 'fos_user_registration_check_email';
+            } else {
+                $authUser = true;
+                $route = 'fos_user_security_login';
+            }
+
+            $this->setFlash('fos_user_success', 'registration.flash.user_created');
+            $url = $this->container->get('router')->generate($route);
+            $response = new RedirectResponse($url);
+
+            if ($authUser) {
+                $this->authenticateUser($user, $response);
+            }
+           
+            return $response;
+        }
+
+        return $this->container->get('templating')->renderResponse('MIWPublicBundle:Registration:register.html.' . $this->getEngine(), array(
+                    'form' => $form->createView(),
+        ));
     }
-    
-    /**
-     * @Route("/conectarse")
-     * @Template("MIWPublicBundle:User:login.html.twig");
-     */
-    public function loginAction()
-    {
-        return array();
-    }
+
 }
