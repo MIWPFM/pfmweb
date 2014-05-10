6366883d14af9757d943023bc07e00ba0b940b84
lienvaldesMIW <lien.valdesi@alumnos.upm.es>
2014-05-10 23:26:21 +0200

    Modificada la fixture de los Deportes

--- src/MIW/DataAccessBundle/DataFixtures/ODM/LoadSports.php
+++ src/MIW/DataAccessBundle/DataFixtures/ODM/LoadSports.php
@@ -20,33 +20,45 @@ class LoadSports extends AbstractFixture implements OrderedFixtureInterface,Cont
         $this->container = $container;
     }
 
-
     public function getOrder(){
         return 1;
     }
+    
    /* php app/console doctrine:mongodb:fixtures:load --fixtures src/MIW/DataAccessBundle/DataFixtures/ODM */
     public function load(ObjectManager $manager){
         print_r("Loading Sports\n");
         $football = new Sport();
-        $football->setName("Futbol 11");
-        $football->setDescription("Juego de pelota blabla");
-        $football->setMinPlayers(11);
-        $football->setAttributes(array('level'=>array('Aficionado','Intermedio','Profesional'),
-                                'position'=>array('Portero','Defensa','Centrocampista','Delantero')));
+        $football->setName("Futbol");
+        $football->setDescription("Real Madrid - Barca");
+        $football->setMinPlayers(12);
+        $football->setLevel("Profesional");
+        $football->setAttributes(array('position'=>array('Portero','Defensa','Centrocampista','Delantero')));
        
         $manager->persist($football);
+    	$manager->flush();
+        
+        $beisbol = new Sport();
+        $beisbol->setName("Beisbol");
+        $beisbol->setDescription("CUBA - USA");
+        $beisbol->setMinPlayers(9);
+        $beisbol->setLevel("Amateurs");
+        $beisbol->setAttributes(array('position'=>array('1Base','2Base','3Base','Pitcher')));
+       
+        $manager->persist($beisbol);
     	$manager->flush();
         
         $paddel = new Sport();
         $paddel->setName("Padel");
         $paddel->setDescription("Juego de raquetas");
         $paddel->setMinPlayers(2);
+        $paddel->setLevel("Aficionado");
         $paddel->setAttributes(array('level'=>array('Aficionado','Intermedio','Profesional')));
        
         $manager->persist($paddel);
     	$manager->flush();
 
         $this->addReference('football', $football);
+        $this->addReference('beisbol', $beisbol);
         $this->addReference('paddel', $paddel);
     }
 }
