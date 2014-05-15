7877891fb45456c9b757a17c7d5daa9965d6f415
7Alberto7 <j.albertocg86@gmail.com>
2014-05-15 17:20:37 +0200

    Arreglado fallo en Mis Deportes cuando el usuario no tiene deportes.

--- src/MIW/IntranetBundle/Controller/MyProfileController.php
+++ src/MIW/IntranetBundle/Controller/MyProfileController.php
@@ -18,41 +18,40 @@ use MIW\IntranetBundle\Form\Type\UserType;
 use MIW\IntranetBundle\Form\Type\AddressType;
 use MIW\IntranetBundle\Form\Type\PasswordType;
 
-class MyProfileController extends Controller
-{
+class MyProfileController extends Controller {
+
     /**
      * @Route("/mis-datos",name="intranet_myprofile_info")
      * @Template("MIWIntranetBundle:MyProfile:myInfo.html.twig")
      */
-    public function viewMyInfoAction(Request $request)
-    {
+    public function viewMyInfoAction(Request $request) {
         $user = $this->get('security.context')->getToken()->getUser();
         $address = $user->getAddress();
-        if($address == null) {
+        if ($address == null) {
             $address = new Address();
         }
-        
+
         // create forms
         $formUser = $this->createForm(new UserType(), $user);
         $formAddress = $this->createForm(new AddressType(), $address);
         $formPassword = $this->createForm(new PasswordType());
-        
-        if($request->getMethod() == "POST"){
-            $dm = $this->get('doctrine.odm.mongodb.document_manager'); 
+
+        if ($request->getMethod() == "POST") {
+            $dm = $this->get('doctrine.odm.mongodb.document_manager');
             $formUser->handleRequest($request);
             $formAddress->handleRequest($request);
             $formPassword->handleRequest($request);
-            
-            if ($formUser->isValid()) {  
+
+            if ($formUser->isValid()) {
                 $user->setName($formUser->get('name')->getData());
                 $user->setBirthday($formUser->get('birthday')->getData());
-                $user->setEmail($formUser->get('email')->getData());                
+                $user->setEmail($formUser->get('email')->getData());
                 $dm->persist($user);
-                $dm->flush();                
+                $dm->flush();
                 return $this->redirect($this->generateUrl('intranet_myprofile_info'));
-           }   
-           
-           if ($formAddress->isValid()) {  
+            }
+
+            if ($formAddress->isValid()) {
                 $address->setAddress($formAddress->get('address')->getData());
                 $address->setCommunity($formAddress->get('community')->getData());
                 $address->setProvince($formAddress->get('province')->getData());
@@ -60,131 +59,127 @@ class MyProfileController extends Controller
                 $address->setZipcode($formAddress->get('zipcode')->getData());
                 $address->setLat($formAddress->get('lat')->getData());
                 $address->setLong($formAddress->get('long')->getData());
-                $user->setAddress($address);                
+                $user->setAddress($address);
                 $dm->persist($user);
-                $dm->flush();                
+                $dm->flush();
                 return $this->redirect($this->generateUrl('intranet_myprofile_info'));
-           }
-           
-           if ($formPassword->isValid()) {  
+            }
+
+            if ($formPassword->isValid()) {
                 $factory = $this->get('security.encoder_factory');
                 $encoder = $factory->getEncoder($user);
                 $password = $encoder->encodePassword($formPassword->get('passwordNew')->getData(), $user->getSalt());
                 $user->setPassword($password);
                 $dm->persist($user);
-                $dm->flush();                
+                $dm->flush();
                 return $this->redirect($this->generateUrl('intranet_myprofile_info'));
-           }
+            }
         }
-        
-        return array('formUser'=>$formUser->createView(), 
-                    'formAddress'=>$formAddress->createView(),
-                    'formPassword'=>$formPassword->createView());
+
+        return array('formUser' => $formUser->createView(),
+            'formAddress' => $formAddress->createView(),
+            'formPassword' => $formPassword->createView());
     }
-    
+
     /**
      * @Route("/mis-deportes",name="intranet_myprofile_sports")
      * @Template("MIWIntranetBundle:MyProfile:mySports.html.twig")
      */
-    public function viewMySportsAction()
-    {
+    public function viewMySportsAction() {
         $user = $this->get('security.context')->getToken()->getUser();
         $dm = $this->get('doctrine.odm.mongodb.document_manager');
         $arraySports = array();
         $mySports = $user->getSports();
         $level = 0;
         $position = null;
-        
-        foreach ($mySports as $obj_key=>$array){
-            $sport = $dm->getRepository('MIWDataAccessBundle:Sport')->find($obj_key);
-            foreach ($array as $key=>$value){
-                switch($key) {
-                    case "level": 
-                        $level = $value;
-                        break;
-                    case "position": 
-                        $position = $value;
-                        break;
+
+        if (!empty($mySports)) {
+            foreach ($mySports as $obj_key => $array) {
+                $sport = $dm->getRepository('MIWDataAccessBundle:Sport')->find($obj_key);
+                foreach ($array as $key => $value) {
+                    switch ($key) {
+                        case "level":
+                            $level = $value;
+                            break;
+                        case "position":
+                            $position = $value;
+                            break;
+                    }
                 }
+                $arraySports[] = array(
+                    'id' => $obj_key,
+                    'name' => $sport->getName(),
+                    'attributes' => $sport->getAttributes(),
+                    'level' => $level,
+                    'position' => $position
+                );
             }
-            $arraySports[] = array(
-                'id' => $obj_key,
-                'name' => $sport->getName(),
-                'attributes' => $sport->getAttributes(),
-                'level' => $level,
-                'position' => $position
-            );
         }
-        
-        return array('mySports'=>$arraySports);
+
+        return array('mySports' => $arraySports);
     }
-    
+
     /**
      * @Route("/mis-partidos",name="intranet_myprofile_games")
      * @Template("MIWIntranetBundle:MyProfile:myGames.html.twig")
      */
-    public function viewMyGamesAction()
-    {
+    public function viewMyGamesAction() {
         $user = $this->get('security.context')->getToken()->getUser();
-        
+
         $dm = $this->get('doctrine.odm.mongodb.document_manager');
         $ownerGames = $dm->getRepository('MIWDataAccessBundle:Game')->findUserGames($user);
         $playingGames = $dm->getRepository('MIWDataAccessBundle:Game')->findPlayingGames($user);
         $playedGames = $dm->getRepository('MIWDataAccessBundle:Game')->findPlayedGames($user);
-            
-        return array('ownerGames'=>$ownerGames,
-                    'playingGames'=>$playingGames,
-                    'playedGames'=>$playedGames);
+
+        return array('ownerGames' => $ownerGames,
+            'playingGames' => $playingGames,
+            'playedGames' => $playedGames);
     }
-    
+
     /**
      * @Route("/mis-partidos/organizados",name="intranet_myprofile_games_owner")
      * @Template("MIWIntranetBundle:MyProfile:ownerGames.html.twig")
      */
-    public function viewMyOwnerGamesAction()
-    {
+    public function viewMyOwnerGamesAction() {
         $user = $this->get('security.context')->getToken()->getUser();
-        
+
         $dm = $this->get('doctrine.odm.mongodb.document_manager');
-        $ownerGames = $dm->getRepository('MIWDataAccessBundle:Game')->findSports($user);       
-            
-        return array('ownerGames'=>$ownerGames);
+        $ownerGames = $dm->getRepository('MIWDataAccessBundle:Game')->findSports($user);
+
+        return array('ownerGames' => $ownerGames);
     }
-    
-        /**
+
+    /**
      * @Route("/mis-partidos/por-jugar",name="intranet_myprofile_games_playing")
      * @Template("MIWIntranetBundle:MyProfile:playingGames.html.twig")
      */
-    public function viewMyPlayingGamesAction()
-    {
+    public function viewMyPlayingGamesAction() {
         $user = $this->get('security.context')->getToken()->getUser();
-        
-        $dm = $this->get('doctrine.odm.mongodb.document_manager');       
+
+        $dm = $this->get('doctrine.odm.mongodb.document_manager');
         $playingGames = $dm->getRepository('MIWDataAccessBundle:Game')->findPlayingGames($user);
-            
-        return array('playingGames'=>$playingGames);
+
+        return array('playingGames' => $playingGames);
     }
-    
+
     /**
      * @Route("/mis-partidos/jugados",name="intranet_myprofile_games_played")
      * @Template("MIWIntranetBundle:MyProfile:playedGames.html.twig")
      */
-    public function viewMyPlayedGamesAction()
-    {
+    public function viewMyPlayedGamesAction() {
         $user = $this->get('security.context')->getToken()->getUser();
-        
-        $dm = $this->get('doctrine.odm.mongodb.document_manager');       
+
+        $dm = $this->get('doctrine.odm.mongodb.document_manager');
         $playedGames = $dm->getRepository('MIWDataAccessBundle:Game')->findPlayedGames($user);
-            
-        return array('playedGames'=>$playedGames);
+
+        return array('playedGames' => $playedGames);
     }
-    
+
     /**
      * @Route("/mis-mensajes",name="intranet_myprofile_messages")
      * @Template("MIWIntranetBundle:MyProfile:myMessages.html.twig")
      */
-    public function viewMyMessagesAction()
-    {
+    public function viewMyMessagesAction() {
         return array();
     }
 
