b6e87058ac6375b7d28616f6d57d9dedb4d696f3
Adrian <alonsus91@gmail.com>
2014-06-08 23:06:06 +0200

    admin fix

--- src/MIW/BackendBundle/MIWBackendBundle.php
+++ src/MIW/BackendBundle/MIWBackendBundle.php
@@ -6,4 +6,9 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MIWBackendBundle extends Bundle
 {
+    
+    public function getParent()
+    {
+        return 'SonataAdminBundle';
+    }
 }
